f673796e1baf51a2687f30600a29fd923175950e
critter1 <critter@uvic.ca>
2016-03-28 22:47:22 -0400

    Added first plot notebooks for sygma paper

---
+++ start_notebook.sh
@@ -0,0 +1,9 @@
+#set the path to your NUPYCEE dir
+export SYGMADIR=/media/christian/ExtDrive64/NuGrid/NUPYCEE6
+
+#export path
+export PYTHONPATH=$PYTHONPATH:$SYGMADIR
+
+#starts notebook server
+jupyter notebook
+
